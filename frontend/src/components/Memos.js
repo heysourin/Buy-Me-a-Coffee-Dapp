ddb35e3ed38271acae375a23aaebe31d2e529332
heysourin <sourin07@gmail.com>
2023-01-04 17:21:18 +0530

    Updated

--- frontend/src/components/Memos.js
+++ frontend/src/components/Memos.js
@@ -1,9 +1,37 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
-const Memos = () => {
+const Memos = ({ state }) => {
+  const [memos, setMemos] = useState([]);
+  const { contract } = state;
+
+  useEffect(() => {
+    const memosMessage = async () => {
+      const memos = await contract.getMemos();
+      setMemos(memos);
+    };
+    contract && memosMessage();
+  }, [contract],);
+  console.log(memos);
   return (
-    <div>Memos</div>
-  )
-}
+    <>
+      <p>Messages:</p>
+      {memos.map((memo) => {
+        return(
+        <table key={memo.timestamp}>
+          <tbody>
+            <tr>
+              <td>{memo.name}</td>
+              <td>{memo.message}</td>
+              <td>{String(memo.timestamp)}</td>
+              <td>{memo.from}</td>
+            </tr>
+          </tbody>
+        </table>
+        )
+      })}
+    </>
+  );
+};
 
-export default Memos
+export default Memos;
